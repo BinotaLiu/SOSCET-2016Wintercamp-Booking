691b12a1c85f1876a6f4437d814ee994afba7e6a
Git Nobody <nobody@soscet.org>
2015-11-22 16:27:59 +0800

    Init

---
+++ app/config/config.example.php
@@ -0,0 +1,65 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+|--------------------------------------------------------------------------
+| Encryption Key
+|--------------------------------------------------------------------------
+|
+| If you use the Encryption class, you must set an encryption key.
+| See the user guide for more info.
+|
+| http://codeigniter.com/user_guide/libraries/encryption.html
+|
+*/
+$config['encryption_key'] = '';
+
+/* DO NOT EDIT FOLLOWING CONFIGURES */
+$config['base_url'] = '';
+$config['index_page'] = '';
+$config['uri_protocol']	= 'REQUEST_URI';
+$config['url_suffix'] = '/';
+$config['language']	= 'traditional-chinese';
+$config['charset'] = 'UTF-8';
+$config['enable_hooks'] = FALSE;
+$config['subclass_prefix'] = 'MY_';
+$config['composer_autoload'] = FALSE;
+$config['permitted_uri_chars'] = 'a-z 0-9~%.:_\-';
+$config['allow_get_array'] = TRUE;
+$config['enable_query_strings'] = FALSE;
+$config['controller_trigger'] = 'c';
+$config['function_trigger'] = 'm';
+$config['directory_trigger'] = 'd';
+$config['log_threshold'] = 0;
+$config['log_path'] = '';
+$config['log_file_extension'] = '';
+$config['log_file_permissions'] = 0644;
+$config['log_date_format'] = 'Y-m-d H:i:s';
+$config['error_views_path'] = '';
+$config['cache_path'] = '';
+$config['cache_query_string'] = FALSE;
+$config['sess_driver'] = 'files';
+$config['sess_cookie_name'] = 'MOUGE_SESS';
+$config['sess_expiration'] = 7200;
+$config['sess_save_path'] = NULL;
+$config['sess_match_ip'] = FALSE;
+$config['sess_time_to_update'] = 300;
+$config['sess_regenerate_destroy'] = FALSE;
+$config['cookie_prefix']	= '';
+$config['cookie_domain']	= '';
+$config['cookie_path']		= '/';
+$config['cookie_secure']	= FALSE;
+$config['cookie_httponly'] 	= FALSE;
+$config['standardize_newlines'] = FALSE;
+$config['global_xss_filtering'] = FALSE;
+$config['csrf_protection'] = FALSE;
+$config['csrf_token_name'] = 'MOUGE_CSRF_TOKEN';
+$config['csrf_cookie_name'] = 'MOUGE_CSRF_PROTECTION';
+$config['csrf_expire'] = 7200;
+$config['csrf_regenerate'] = TRUE;
+$config['csrf_exclude_uris'] = array();
+$config['compress_output'] = FALSE;
+$config['time_reference'] = 'local';
+$config['rewrite_short_tags'] = FALSE;
+$config['proxy_ips'] = '192.168.1.101';
+
