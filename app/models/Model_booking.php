7e3899b51f5d6769ad7f5863e592277fe4355ed8
BinotaLiu <binota@binota.org>
2015-11-23 10:01:43 +0900

    Add Model_booking

---
+++ app/models/Model_booking.php
@@ -0,0 +1,46 @@
+<?php
+
+class Model_Booking extends CI_Model {
+
+    public function addBooking($data) {
+        if(empty($data['created_at'])) $data['created_at'] = time();
+        if(empty($data['modified_at'])) $data['modified_at'] = time();
+        if(empty($data['token'])) $data['token'] = strtolower(random_string('alnum', 32));
+
+        $this->db->insert('bookings', $data);
+
+        $id = $this->db->insert_id();
+        $token = $data['token'];
+
+        return [$id, $token];
+    }
+
+    public function getBooking($id, $token) {
+        $query = $this->db->get('bookings')
+                           ->where(['id' => $id,
+                                    'token' => $token]);
+        return $query->row();
+    }
+
+    public function editBooking($id, $data) {
+        if(empty($data['modified_at'])) $data['modified_at'] = time();
+
+        $this->db->replace('bookings', $data);
+    }
+
+    public function getPayments($booking) {
+        $this->db->where(['booking' => $booking]);
+        $query = $this->db->get('payments');
+
+        return $query->row();
+    }
+
+    public function addPayment($data) {
+        if(empty($data['created_at'])) $data['created_at'] = time();
+        if(empty($data['modified_at'])) $data['modified_at'] = time();
+        $this->db->insert('payments', $data);
+        return $this->db->insert_id();
+    }
+
+}
+
