1e553a95fbd9252d221d400cb5a8ed43f4513672
BinotaLiu <binota@binota.org>
2015-12-08 20:27:38 +0800

    預設密碼

--- app/views/event/login.php
+++ app/views/event/login.php
@@ -4,7 +4,11 @@
   <div class="alert alert-danger">
     <p>密碼錯誤或報名資料網址錯誤</p>
   </div>
+  <?php } else { ?>
   <?php } ?>
+  <div class="alert alert-notice">
+    <p>若您未設定密碼，則請使用預設密碼 123456</p>
+  </div>
   <?php echo form_open(); ?>
     <input type="password" name="password" placeholder="請輸入密碼">
     <button type="submit">送出</button>
