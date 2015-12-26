e478f66f102748b4791e6f9ce322149a723bc65a
BinotaLiu <binota@binota.org>
2015-12-26 13:08:47 +0800

    條款

--- app/views/event/event.php
+++ app/views/event/event.php
@@ -97,13 +97,91 @@
             <div class="alert alert-warning" role="alert">
                 <p><b>注意！</b> 若您未滿 20 歲，緊急聯絡人與保險受益人需為您的監護人！</p>
             </div>
-            <p class="text-right">送出報名表格即代表您同意<a href="#">《個人資料蒐集與利用條款》</a>與<a href="#">《報名須知》</a></p>
+            <p class="text-right">送出報名表格即代表您同意<a href="#" data-toggle="modal" data-target="#modal-pi">《個人資料蒐集與利用條款》</a>與<a href="#" data-toggle="modal" data-target="#modal-tos">《報名須知》</a></p>
             <div class="pull-right">
                 <button type="submit" class="btn btn-lg btn-success">確認無誤，立即報名！</button>
            </div>
         </div>
     </form>
 </section>
+
+<div class="modal fade" id="modal-pi" tabindex="-1" role="dialog" aria-labelledby="modal-pi-label">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="modal-pi-label">個人資料蒐集與利用條款</h4>
+            </div>
+            <div class="modal-body">
+<h4>前言</h4><br>
+  本條款係針對主辦單位（國立東華大學資訊工程學系與東部開源學生社群，下簡稱甲方）利用報名者（下簡稱乙方）之個人資料（下簡稱個資）時之說明，乙方報名時應詳閱並同意本條款之內容。<br>
+  本條款之訂定符合我國《個人資料保護法》，如有未盡事宜，以該法律為主。<br>
+  依照我國《民法》規定，若乙方為未滿二十歲之人則視為限制行為能力人，則參與甲方提供之活動時應經乙方法定監護人之同意，且乙方法定監護人需詳閱並同意本條款之內容。<br>
+<br>
+<h4>條款期間與條款之終止</h4><br>
+  甲方於條款期間若因作業需要則可使用乙方個資。<br>
+  甲方若於條款期間因作業需要提供第三方乙方之個人資料，亦遵守本條款，請參閱本條款之「提供第三方之個人資料」節。<br>
+  本活動作業結束即視為本條款之終止<br>
+  依照我國《個人資料保護法》之規定，活動期間乙方有權隨時要求甲方停止蒐集、利用乙方個資之權利，乙方要求甲方刪除個人資料的同時，亦視為本條款之終止，同時也視為放棄參與甲方所舉辦之活動。<br>
+  如遇乙方死亡則視為本契約終止。<br>
+<br>
+<h4>提供第三方之個人資料</h4><br>
+  甲方因活動需要需提供下列個資給予特定之第三方單位：<br>
+    教育部補助扎根高中職資訊科學教育計畫：姓名、所屬學校、學生證影本<br>
+    教育部資訊志工計畫東華大學學習科技資訊志工團隊：姓名、東華大學會館、姓名<br>
+    保險公司：姓名、身份證字號、出生年月日、、保險受益人、保險受益人關係<br>
+<br>
+<h4>免責事項</h4><br>
+  若因不可抗因素導致甲方存有之乙方之個人資料遭不法之第三者非法存取，甲方將全力協助乙方通過法律途徑索取賠償。<br>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="modal-tos" tabindex="-1" role="dialog" aria-labelledby="modal-tos-label">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="modal-tos-label">報名須知</h4>
+            </div>
+            <div class="modal-body">
+哈囉！花蓮高工、花蓮高商、花蓮高農的同學們大家好！東華大學資訊扎根計畫為了鼓勵花蓮的高職生們積極參與資訊冬令營，<br>
+所以我們推出了只有三校學生獨享的優惠方案哦！<br>
+<br>
+4人一起報名，每人可減免400元<br>
+6人一起報名，每人可減免600元<br>
+8人一起報名，每人可減免800元<br>
+（小提醒：這個優惠可以和一般團體報名方案一起使用不衝突，但是只有三校學生才有減免哦！）<br>
+例如：<br>
+2 名花商學生 + 2 其他校學生 = 4人團體報名 + 2 人三校方案 （也就是他校學生報名費為 3000，花商學生報名費為 2600 哦！）<br>
+<br>
+<h4>取消報名</h4><br>
+哈囉，臨時有事沒辦法參加營隊的朋友，請參照下列的說明進行退費喔！<br>
+<br>
+<h5>信箱取消報名</h5><br>
+寄信至 contact@soscet.org<br>
+標題請用「我要取消報名開源！資訊萌芽營！」<br>
+註明  **學員姓名**、**身分證字號末五碼**、**我要取消報名**、**退費匯款戶頭**<br>
+<br>
+並靜待一至二個工作天工作人員就會回信跟您確認退費手續之後就完成囉！<br>
+匯款成功後我們會寄一封確認信至您的信箱，也請關注我們的回信、確認匯款成功哦！<br>
+<br>
+歡迎在信中寫下對我們的任何意見，非常感謝。<br>
+<br>
+<h5>電話取消報名</h5><br>
+連絡人： <br>
+傅柏軒 0975-383-086<br>
+林育慈 0953-381-533<br>
+周孟潔 0931-111-127<br>
+<br>
+請在電話一開始就說明來意「我要取消報名開源！資訊萌芽營！」<br>
+並且告知 **學員姓名**、**身分證字號末五碼**、**退費匯款戶頭**、**如何通知已經匯款**<br>
+這樣就可以了喔！<br>
+            </div>
+        </div>
+    </div>
+</div>
 
 <script>
   $(function() {
